9405c8b449f4a6fe83311f1da0f8af162ef87f21
Soner Aktas <soneraktas01@gmail.com>
2026-05-27 13:56:43 +0300

    taban aritmetiği.mp3 çalınıyor

--- src/main/java/com/soner/tabanaritmatigi/Launcher.java
+++ src/main/java/com/soner/tabanaritmatigi/Launcher.java
@@ -4,16 +4,27 @@ import javafx.application.Application;
 
 
 public class Launcher {
+    /*
+    Java'da ProcessBuilder kullanırken karşılaştığın bu durumun iki temel sebebi var: Tilde (~) karakteri ve komutun parçalanma şekli.
+
+    Terminalde ~ yazdığında bunu senin "home" klasörüne (/home/soner) Linux kabuğu (shell) çevirir. Ancak Java doğrudan işletim sistemiyle konuştuğu için ~ sembolünün ne anlama geldiğini bilmez ve bu yolu bulamaz.
+     */
 
     public static void main(String[] args) {
 
-        ProcessBuilder pb = new ProcessBuilder("espeak", "-v", "tr", "-s", "150", "-g", "10", "mab vildan");
+        // Dosya yolunu tam olarak belirtiyoruz
+        String homeDir = System.getProperty("user.home");
+        String sesYolu = homeDir + "/sesler/turkceSes/tabanAritmetigi.mp3";
+
+        ProcessBuilder pb = new ProcessBuilder("mpg123", sesYolu);
         try {
             pb.start();
+            // Ses bitene kadar beklemesini istersen (isteğe bağlı):
+            // p.waitFor();
         } catch (Exception e) {
-            System.out.println("ses dosyası çalınırken hata oluştu");
-            //throw new RuntimeException(e);
-        }
+            //System.out.println("ses dosyası çalınırken hata oluştu");
+            System.out.println("Ses dosyası çalınırken hata oluştu: " + e.getMessage());
+        }//end catch
 
         Application.launch(HelloApplication.class, args);
     }//end main
